eb706020f50086d7c83fdf88ec09fbf33ece366e
goptun <ramos.matheus@icloud.com>
2023-09-18 21:30:26 -0300

    [M2S02] Ex Extra 02 - Atendimento Automatizado

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ### Ex Extra 01 - Maior Valor
 
 Receber 3 valores de entrada do usuário e retornar o maior deles.
+
+### Ex Extra 02 - Atendimento Automatizado
+
+Uma empresa está implantando um sistema automatizado de atendimento aos clientes e fornecedores, ao estilo WhatsApp. Os setores da empresa são divididos em 3 :
+Depto Comercial;
+Depto Financeiro;
+Depto RH;
+Faça um programa que imprima na tela ao usuário suas opções e logo depois imprima qual foi a opção escolhida pelo usuário
---
+++ src/AtendimentoAutomatizado.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AtendimentoAutomatizado {
+  public static void main(String[] args) {
+    Scanner user = new Scanner(System.in);
+
+    int opcao;
+
+    System.out.println("Bem-vindo ao sistema de atendimento automatizado!");
+    System.out.println("Escolha um departamento:");
+    System.out.println("1. Depto Comercial");
+    System.out.println("2. Depto Financeiro");
+    System.out.println("3. Depto RH");
+
+    System.out.print("Digite o número do deparmento desejado:");
+    opcao = user.nextInt();
+
+    switch (opcao) {
+      case 1:
+        System.out.println("Você selecionou o Depto Comercial");
+        break;
+      case 2:
+        System.out.println("Você selecionou o Depto Financeiro");
+        break;
+      case 3: 
+        System.out.println("Você selecionou o Depto RH");
+        break;
+      default:
+          System.out.println("Por favor, escolha uma opção válida!");
+          break;
+    }
+    user.close();
+  }
+}
