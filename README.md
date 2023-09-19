a9bd93e3a78d80fbaab2503a251bf3411e757931
goptun <ramos.matheus@icloud.com>
2023-09-18 22:03:20 -0300

    [M2S02] Ex Extra 04 - Calculo Aprovação

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Faça um programa que imprima na tela ao usuário suas opções e logo depois im
 ### Ex Extra 03 - Calculo Média
 
 Solicite ao usuário 3 notas, de 0.0 a 10.0. Após a terceira nota, calcule a média e apresente o resultado de aprovação ou reprovação. Para aprovação, o aluno deve possuir média superior a 7.0.
+
+### Ex Extra 04 - Calculo Aprovação
+
+Solicite ao usuário 3 notas, de 0.0 a 10.0, e também o número de faltas. Então calcule a média e a frequência do aluno. Informe ao usuário se o aluno está aprovado ou reprovado, considerando que para ser aprovado o aluno precisa ter média superior a 7.0 e frequência superior a 75%. Se qualquer um desses requisitos não for atendido, o aluno está reprovado.
---
+++ src/CalculoAprovacao.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculoAprovacao {
+  public static void main(String[] args) {
+    Scanner user = new Scanner(System.in);
+
+    System.out.print("Digite a primeira nota: ");
+    double nota1 = user.nextDouble();
+
+    System.out.print("Digite a segunda nota: ");
+    double nota2 = user.nextDouble();
+
+    System.out.print("Digite a terceira nota: ");
+    double nota3 = user.nextDouble();
+
+    System.out.print("Digite o número de faltas: ");
+    int faltas = user.nextInt();
+
+    double media = (nota1 + nota2 + nota3) / 3.0;
+
+    double frequencia = ((double) (100 - faltas) / 100) * 100;
+
+    if (media > 7.0 && frequencia > 75.0) {
+      System.out.println("Aluno aprovado!");
+    } else {
+      System.out.println("Aluno reprovado");
+    }
+
+    System.out.printf("Média: %.1f%n", media);
+    System.out.printf("Frequência: %.1f%%\n", frequencia);
+
+    user.close();
+  }
+}
